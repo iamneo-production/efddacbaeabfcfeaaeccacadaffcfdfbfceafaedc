640919a65fe36bc171a425f5820579d760f56590
Iamneo <techadmin@iamneo.ai>
2023-08-03 16:09:06 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/model1/task.java
+++ springapp/springapp/src/main/java/com/examly/model1/task.java
@@ -8,6 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.Column;
 
 @Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
 public class task 
 {
     @Id
@@ -18,6 +22,37 @@ public class task
     private Date taskDate;
     private String taskName;
     private String taskStatus;
+    
+    public Long getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public Date getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(Date taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
   
     
 }
---
+++ springapp/springapp/src/main/java/com/examly/services/Taskservice.java
@@ -0,0 +1,8 @@
+package com.examly.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Taskservice{
+    
+}
