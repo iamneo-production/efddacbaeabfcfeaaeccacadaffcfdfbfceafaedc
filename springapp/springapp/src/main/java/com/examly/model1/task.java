874195bdd600a8218997ccb006e57d7cedf74e78
Iamneo <techadmin@iamneo.ai>
2023-08-03 15:39:05 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/model1/task.java
+++ springapp/springapp/src/main/java/com/examly/model1/task.java
@@ -1,11 +1,23 @@
 package com.examly.model1;
 
 import java.util.Date;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity
-public class task {
+public class task 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="Id")
+    private Long id;
+    private String title;
+    private String type;
+    private Date dueDate;
+    private String description;
   
     
 }
--- springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan("com.examly.model1")
 public class SpringappApplication {
 
 	public static void main(String[] args) {
