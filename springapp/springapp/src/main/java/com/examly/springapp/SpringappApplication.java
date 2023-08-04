cee85c3d0b2d1d47886d1cc704db7f5409336cd2
Iamneo <techadmin@iamneo.ai>
2023-08-04 06:29:49 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -3,8 +3,8 @@ package com.examly.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@EntityScan("com.examly")
+@SpringBootApplication(scanBasePackages={
+	"com.examly.sbproject"})
 public class SpringappApplication {
 
 	public static void main(String[] args) {
