ee4481a2a15342f5ac9ee803220b95f12273d2f9
Iamneo <techadmin@iamneo.ai>
2023-08-04 06:44:49 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
+++ springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
@@ -1,22 +1,25 @@
 package com.examly.controllers;
 
+import java.util.List;
+
 import com.examly.services.Taskservice;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin("*")
-@AllArgsConstructor
 public class Taskcontroller {
     
     private Taskservice taskservice;
 
+    @GetMapping("/tasks")
     public List<task> getTask()
     {
-        return task
+        return taskservice.getTasks();
 
     }
 }
