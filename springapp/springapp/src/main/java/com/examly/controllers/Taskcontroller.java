ce40b79c1db6cd7c3427a08c8ae4db039e732c35
Iamneo <techadmin@iamneo.ai>
2023-08-07 12:18:56 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
+++ springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class Taskcontroller {
     private Taskservice taskservice;
     private Taskrepository taskRepository;
 
-    @PostMapping("/saveTask")
+    @PutMapping("/saveTask")
     public ResponseEntity<?> saveTask(@RequestBody Task taskPara,@PathVariable Long id)
     {
         if(taskservice.existById(id))
@@ -47,6 +48,12 @@ public class Taskcontroller {
         }
     }
 
+    @PostMapping("/savetask")
+    public Task saveTask(@RequestBody Task task)
+    {
+        return taskservice.saveTask(task); 
+    }
+
     @GetMapping("/changeStatus/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id,@RequestBody Task task)
     {
@@ -54,24 +61,34 @@ public class Taskcontroller {
         return ResponseEntity.ok(updatedTask);
     }
 
-    @GetMapping("deleteTask/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id)
+    @GetMapping("/deleteTask/{id}")
+        public ResponseEntity<?> deleteTask(@PathVariable Long id)
     {
-        taskservice.deleteTask(id);
-        return ResponseEntity.noContent().build();
-
-    }
+        if(taskservice.existById(id))
+        {
+            taskservice.deleteTask(id);
+            HashMap<String,String>message=new HashMap<>();
+            message.put("message",id + "task removed");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
+        else
+        {
+            HashMap<String,String>message=new HashMap<>();
+            message.put("message",id+"task not fund or matched");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
+        }
 
     @GetMapping("/alltasks")
     public List<Task> getAllTasks()
     {
-        return taskRepository.findAll();
+        return taskservice.findAll();
     }
 
     @GetMapping("/getTask/{id}")
     public Task getTaskById(@PathVariable Long id)
     {
-        return taskRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Task not found with id:" +id)); 
+        return taskservice.findById(id).orElseThrow(()->new EntityNotFoundException("Task not found with id:" +id)); 
     }
 }
 
