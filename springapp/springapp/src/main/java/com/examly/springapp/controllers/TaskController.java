33b601c47c06fe3e0700c3f9a9302c1b9f5ca8bd
Iamneo <techadmin@iamneo.ai>
2023-08-08 13:42:52 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
+++ springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
@@ -1,12 +1,40 @@
 package com.examly.controllers;
 
-public class TaskController {
 
-    Task task = new Task();
-    task.setTitle("Learn Spring Boot");
-    task.setDescription("This task is to learn Spring Boot");
-    task.setCompleted(false);
-    TaskController controller = new TaskController();
-    controller.createTask(task);
-    
+@RestController
+@RequestMapping("/tasks")
+public class TaskController 
+{
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) 
+    {
+        this.taskService = taskService;
+    }
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) 
+    {
+        Task savedTask = taskService.createTask(task);
+        return ResponseEntity.ok(savedTask);
+    }
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+    @GetMapping("/getTask/{taskId}")
+    public ResponseEntity<Task> getTask(@PathVariable Long taskId) {
+        Task task = taskService.getTask(taskId);
+        return ResponseEntity.ok(task);
+    }
+    @PutMapping("/changeStatus/{taskId}")
+    public ResponseEntity<Task> changeStatus(@PathVariable Long taskId, @RequestBody Task task) {
+        Task updatedTask = taskService.changeStatus(taskId, task);
+        return ResponseEntity.ok(updatedTask);
+    }
+    @DeleteMapping("/deleteTask/{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
+        taskService.deleteTask(taskId);
+        return ResponseEntity.ok().build();
+    }
 }
+    
--- springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
+++ springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
@@ -10,4 +10,6 @@ public class TaskService
     @Autowired
     private TaskRepository taskRepository;
 
+    
+
 }
