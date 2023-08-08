b8d78a84033636b9e74e68663adbf77acfcc50e1
Iamneo <techadmin@iamneo.ai>
2023-08-08 14:06:06 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
+++ springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -1,6 +1,12 @@
-package com.examly.springapp.controllers;
+package com.examly.springapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Task 
 {
+    @Id
 
     
 }
--- springapp/springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
+++ springapp/springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
@@ -1,5 +1,10 @@
 package com.examly.springapp.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>
+{
     
 }
