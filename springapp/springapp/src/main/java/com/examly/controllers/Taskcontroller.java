35b2fccdb2bdff5793dc8a0f015f95c5046d1623
Iamneo <techadmin@iamneo.ai>
2023-08-08 12:52:26 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
+++ springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
@@ -3,12 +3,15 @@ package com.examly.controllers;
 import javax.persistence.EntityNotFoundException;
 import com.examly.model.Task;
 import java.util.HashMap;
+import java.util.Optional;
+
 import com.examly.services.Taskservice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +25,10 @@ public class Taskcontroller {
     private Taskservice taskservice;
 
     @GetMapping("/alltasks")
-    public ResponseEntity<?> alltasks(@RequestBody Task taskPara,@PathVariable Long id) {
-        if(taskservice.existById(id))
+    public ResponseEntity<?> alltasks(@RequestBody Task taskPara,@PathVariable Long taskId) {
+        if(taskservice.existById(taskId))
         {
-            Task task=taskservice.getTaskById(id).orElseThrow(()->new EntityNotFoundException("Requested Task not found"));
+            Task task=taskservice.getTaskById(taskId).orElseThrow(()->new EntityNotFoundException("Requested Task not found"));
             task.setTaskHolderName(taskPara.getTaskHolderName());
             task.setTaskDate(taskPara.getTaskDate());
             task.setTaskName(taskPara.getTaskName());
@@ -36,7 +39,7 @@ public class Taskcontroller {
         else
         {
             HashMap<String,String>message=new HashMap<>();
-            message.put("message",id+"task not fund or matched");
+            message.put("message",taskId+"task not fund or matched");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
         }
     }
@@ -47,7 +50,17 @@ public class Taskcontroller {
         return taskservice.saveTask(task); 
     }
 
-    
+    @DeleteMapping("/deleteTask/{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
+        taskservice.deleteTask(taskId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById(@PathVariable Long id) 
+    {
+    return taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found with id: " + id));
+}
     
     
 }
