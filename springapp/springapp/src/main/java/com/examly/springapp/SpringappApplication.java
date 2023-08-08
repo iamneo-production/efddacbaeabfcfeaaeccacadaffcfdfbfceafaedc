e7dd6ab7785bb818b4cc8edd8c60a44789ab7796
Iamneo <techadmin@iamneo.ai>
2023-08-08 06:19:05 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@Ent
 @ComponentScan(basePackages= {"com.examly"})
+
 public class SpringappApplication {
 
 	public static void main(String[] args) {
