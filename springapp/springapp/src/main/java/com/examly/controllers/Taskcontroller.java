122ac27fd59af1adc0093e592e3d00005f81ed35
Iamneo <techadmin@iamneo.ai>
2023-08-08 09:35:48 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
+++ springapp/springapp/src/main/java/com/examly/controllers/Taskcontroller.java
@@ -1,13 +1,12 @@
 package com.examly.controllers;
 
-import ent
+import javax.persistence.EntityNotFoundException;
 import com.examly.model.Task;
 import java.util.HashMap;
 import com.examly.services.Taskservice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,12 +21,11 @@ public class Taskcontroller {
     @Autowired
     private Taskservice taskservice;
 
-    @PutMapping("/")
-    public ResponseEntity<Task> addTask(@RequestBody Task taskPara,@PathVariable Long id) {
+    @PutMapping("/alltasks")
+    public ResponseEntity<?> alltasks(@RequestBody Task taskPara,@PathVariable Long id) {
         if(taskservice.existById(id))
         {
             Task task=taskservice.getTaskById(id).orElseThrow(()->new EntityNotFoundException("Requested Task not found"));
-            task.setTaskId(taskPara.getTaskId());
             task.setTaskHolderName(taskPara.getTaskHolderName());
             task.setTaskDate(taskPara.getTaskDate());
             task.setTaskName(taskPara.getTaskName());
@@ -43,12 +41,17 @@ public class Taskcontroller {
         }
     }
 
-    @PostMapping("/task")
-    public Task addTask(@RequestBody Task task)
+    @PostMapping("/saveTask")
+    public Task saveTask(@RequestBody Task task)
     {
         return taskservice.saveTask(task); 
     }
 
+    @RequestMapping(path = "/saveTask", method = RequestMethod.POST)
+    public Task saveTask(@RequestBody Task task)
+    {
+        return taskservice.saveTask(task); 
+    }
     
     
 }
