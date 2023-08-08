194eda028ceeb77a3f251af1df30a9266a4462b3
Iamneo <techadmin@iamneo.ai>
2023-08-08 06:30:06 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,15 +1,10 @@
 package com.examly.springapp;
 
-import javax.persistence.Entity;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@Entity
-@ComponentScan(basePackages= {"com.examly"})
 @EnableAutoConfiguration
 
 public class SpringappApplication {
