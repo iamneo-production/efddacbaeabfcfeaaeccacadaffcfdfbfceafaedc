93f47d3779217e487599327a5802af4a9f0e8fb4
Iamneo <techadmin@iamneo.ai>
2023-08-08 14:18:07 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
+++ springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -1,12 +1,23 @@
 package com.examly.springapp.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Task 
 {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    
+    private Long taskId;
+    private String taskHolderName;
+    private Date taskDate;
+    private String taskName;
+    private String taskStatus;
+
+
 }
