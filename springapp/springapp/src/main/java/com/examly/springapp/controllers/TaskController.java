14edc9e3d2f34c3e5c7a121176b041c5cdc96b00
Iamneo <techadmin@iamneo.ai>
2023-08-09 06:00:53 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
+++ springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
@@ -7,4 +7,57 @@ public class TaskController
 {
     @Autowired
     private TaskService taskService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // Date - dd/MM/yyyy
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+    public String showAddTodoPage(ModelMap model) {
+        model.addAttribute("todo", new Todo());
+        return "todo";
+    }
+
+    @RequestMapping(value = "/deleteTask/{id}", method = RequestMethod.GET)
+    public String deleteTask(@RequestParam long id) {
+        taskService.deleteTask(id);
+        return "redirect:/deleteTask/{id}";
+    }
+
+    @RequestMapping(value = "/chagngeStatus/{id}", method = RequestMethod.GET)
+    public String chagngeStatus(@RequestParam long id, ) {
+        Task task = taskService.getTaskById(id).get();
+        model.put("task", task);
+        return "task";
+    }
+
+    @RequestMapping(value = "/update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "todo";
+        }
+
+        todo.setUserName(getLoggedInUserName(model));
+        todoService.updateTodo(todo);
+        return "redirect:/list-todos";
+    }
+
+    @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+    public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "todo";
+        }
+
+        todo.setUserName(getLoggedInUserName(model));
+        todoService.saveTodo(todo);
+        return "redirect:/list-todos";
+    }
+}
+
+    
 }
