609e677baf21bd4496a2b6ebc4c36c26d7d5f9a0
Iamneo <techadmin@iamneo.ai>
2023-08-09 15:00:18 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
+++ springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
@@ -4,16 +4,14 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.examly.springapp.model.Task;
 import com.examly.springapp.repository.TaskRepository;
+import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService 
 {
     @Autowired
     private TaskRepository taskRepository;
-    public TaskService(TaskRepository taskRepository) 
-    {
-        this.taskRepository = taskRepository;
-    }
+    
 
     public void addTask(String taskHolderName, Date taskDate, String taskName,String taskStatus ) 
     {
