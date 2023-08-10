b45996dcbf430442a919a72fec03a3e909b46365
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:30:03 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
+++ springapp/springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
@@ -2,15 +2,11 @@ package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import com.examly.springapp.model.Task;
-
 
 @Repository
 public interface TaskRepository extends JpaRepository < Task, Integer > 
-{   
-    public Task findByTask(String task);
-    public List<Task> findAll();
+{
+
 }
 
--- springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
+++ springapp/springapp/src/main/java/com/examly/springapp/service/TaskService.java
@@ -1,7 +1,6 @@
 package com.examly.springapp.service;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.examly.springapp.model.Task;
 import com.examly.springapp.repository.TaskRepository;
 import org.springframework.stereotype.Service;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskService 
 {
-    @Autowired
     private TaskRepository taskRepository;
       
     public List<Task> getAllTask() {
