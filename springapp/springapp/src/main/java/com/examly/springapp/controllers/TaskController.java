1beda5fbf0dce2bd076d9bde43a59b55bbbb738f
Iamneo <techadmin@iamneo.ai>
2023-08-10 13:35:44 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
+++ springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
@@ -18,9 +18,26 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
-    @GetMapping("/create")
-    public String createTask(Task task) {
-        task.addAttribute("task", new Task());
-        return "task/create";
+    @PostMapping("/saveTask")
+    public Task addtask(@RequestBody Task task){
+        taskService.save(task);
+        return task;
+    }
+
+    @GetMapping("/alltasks")
+    public List<Task> getTask()
+    {
+        return taskService.getTask();
+    }
+
+    @GetMapping("/getTask/{taskId}")
+    public Optional<Task> getTaskId(@PathVariable Long taskId)
+    {
+        return taskService.getTaskId(taskId);
+    }
+
+    @GetMapping("/deleteTask/{taskid}")
+    public void deleteTask(@PathVariable Long taskid){
+        taskService.deleteTask(taskid);
     }
 }
