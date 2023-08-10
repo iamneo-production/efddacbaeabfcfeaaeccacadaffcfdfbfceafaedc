d574200c973f11d3a62fca27340d584d87cdf902
Iamneo <techadmin@iamneo.ai>
2023-08-10 05:04:15 +0000

    Backup Commit

--- springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
+++ springapp/springapp/src/main/java/com/examly/springapp/controllers/TaskController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.examly.springapp.service.TaskService;
 import com.examly.springapp.model.Task;
 import com.examly.springapp.repository.TaskRepository;
+import com.examly.springapp.service.TaskService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import com.examly.springapp.service.TaskService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
--- springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
+++ springapp/springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Table(name="/taskpro")
 public class Task 
 {
 
